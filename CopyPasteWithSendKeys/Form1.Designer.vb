b85d5bc5118c4536e69f225480d55592c3bdfac6
Marc Bender <marc.andre.bender@hotmail.com>
2019-12-15 16:13:41 +0100

    a fundamentally working version of the code maybe i have to change it later to add or fix something but for now it will do

--- CopyPasteWithSendKeys/Form1.Designer.vb
+++ CopyPasteWithSendKeys/Form1.Designer.vb
@@ -22,17 +22,58 @@ Partial Class Form1
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.goButton = New System.Windows.Forms.Button()
+        Me.inputTextBox = New System.Windows.Forms.TextBox()
+        Me.charsRemainingLabel = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'goButton
+        '
+        Me.goButton.Location = New System.Drawing.Point(13, 415)
+        Me.goButton.Name = "goButton"
+        Me.goButton.Size = New System.Drawing.Size(775, 23)
+        Me.goButton.TabIndex = 1
+        Me.goButton.Text = "Jetzt Tastenanschläge vorgaukeln"
+        Me.goButton.UseVisualStyleBackColor = True
+        '
+        'inputTextBox
+        '
+        Me.inputTextBox.AcceptsReturn = True
+        Me.inputTextBox.AcceptsTab = True
+        Me.inputTextBox.AllowDrop = True
+        Me.inputTextBox.Location = New System.Drawing.Point(13, 13)
+        Me.inputTextBox.MaxLength = 65535
+        Me.inputTextBox.Multiline = True
+        Me.inputTextBox.Name = "inputTextBox"
+        Me.inputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.inputTextBox.Size = New System.Drawing.Size(775, 363)
+        Me.inputTextBox.TabIndex = 2
+        Me.inputTextBox.WordWrap = False
+        '
+        'charsRemainingLabel
+        '
+        Me.charsRemainingLabel.AutoSize = True
+        Me.charsRemainingLabel.Location = New System.Drawing.Point(13, 383)
+        Me.charsRemainingLabel.Name = "charsRemainingLabel"
+        Me.charsRemainingLabel.Size = New System.Drawing.Size(223, 13)
+        Me.charsRemainingLabel.TabIndex = 3
+        Me.charsRemainingLabel.Text = "Verbleibende Textlänge bis Maximum erreicht:"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.charsRemainingLabel)
+        Me.Controls.Add(Me.inputTextBox)
+        Me.Controls.Add(Me.goButton)
         Me.Name = "Form1"
-        Me.Text = "Tastenanschläge faken"
+        Me.Text = "Tastenanschläge vorgaukeln"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
-
+    Friend WithEvents goButton As Button
+    Friend WithEvents inputTextBox As TextBox
+    Friend WithEvents charsRemainingLabel As Label
 End Class
--- CopyPasteWithSendKeys/Form1.vb
+++ CopyPasteWithSendKeys/Form1.vb
@@ -1,3 +1,35 @@
 ﻿Public Class Form1
+    Public Declare Sub Sleep Lib "kernel32" Alias "Sleep" (ByVal dwMilliseconds As Long)
+    ReadOnly charsRemainingLabelDefaultText As String = "Verbleibende Textlänge bis Maximum erreicht: "
+    ReadOnly standinOpeningParenthesis As String = "$$$$$Klammerauf$$$$$"
+    ReadOnly standinClosingParenthesis As String = "$$$$$Klammerzu$$$$$"
+    ReadOnly standinOpeningCurly As String = "$$$$$GeschwungeneKlammerauf$$$$$"
+    ReadOnly standinClosingCurly As String = "$$$$$GeschwungeneKlammerzu$$$$$"
+    ReadOnly standinTilde As String = "$$$$$Tilde$$$$$"
+    Private Sub goButton_Click(sender As Object, e As EventArgs) Handles goButton.Click
+        Dim textToBeSent As String = inputTextBox.Text
+        textToBeSent = textToBeSent.Replace("{", standinOpeningCurly)
+        textToBeSent = textToBeSent.Replace("}", standinClosingCurly)
+        textToBeSent = textToBeSent.Replace(standinOpeningCurly, "{{}")
+        textToBeSent = textToBeSent.Replace(standinClosingCurly, "{}}")
+        textToBeSent = textToBeSent.Replace("(", standinOpeningParenthesis)
+        textToBeSent = textToBeSent.Replace(")", standinClosingParenthesis)
+        textToBeSent = textToBeSent.Replace("+", "{+}")
+        textToBeSent = textToBeSent.Replace("%", "+(5)")
+        textToBeSent = textToBeSent.Replace(standinOpeningParenthesis, "+(8)")
+        textToBeSent = textToBeSent.Replace(standinClosingParenthesis, "+(9)")
 
+        textToBeSent = textToBeSent.Replace(Chr(13) + Chr(10), "~")
+        If inputTextBox.Text.Contains("~") Then
+            MsgBox("Texte die das ~ Symbol beinhalten werden nicht unterstützt" + Chr(10) + "Der Text wird wohl an das andere Fenster übergeben aber die" + Chr(10) + "Tilde wird wohl zu was auch immer die Enter-Taste bewirkt!!!!", MessageBoxButtons.OK, "Tastenanschläge vorgaukeln")
+        End If
+        SendKeys.Send("%{TAB}")
+        Sleep(100)
+        SendKeys.Send(textToBeSent)
+    End Sub
+
+    Private Sub inputTextBox_TextChanged(sender As Object, e As EventArgs) Handles inputTextBox.TextChanged
+        Dim charsRemainingCount As Int64 = inputTextBox.MaxLength - inputTextBox.TextLength
+        charsRemainingLabel.Text = charsRemainingLabelDefaultText + charsRemainingCount.ToString
+    End Sub
 End Class
